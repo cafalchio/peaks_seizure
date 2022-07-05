2c706fa41e8ced92e7286188b5e17999650bb31c
Matheus Cafalchio <47299428+cafalchio@users.noreply.github.com>
2022-07-05 16:05:47 +0100

    fixed but, times were calculated with wrong names, added std, mean amplitude

--- batch_run.py
+++ batch_run.py
@@ -1,11 +1,24 @@
 from utils.utils import *
 # from utils.config import config
 import pandas as pd
+from tqdm import tqdm
+import logging
+import warnings
+warnings.filterwarnings("ignore")
+
+
+
+# log config
+logging.basicConfig(filename="batch_log.txt",
+                    filemode='a',
+                    format='%(message)s',
+                    level=logging.INFO)
+
 
 # Configuration
 PATH = "C:\\Users\\cafa\\Documents\\mark_code\\laura_analysis"
 DF_FILE = "rec_times.csv"
-minutes = 5
+minutes = 10
 
 df_path = os.path.join(PATH, DF_FILE)
 df = read_dataframe(df_path)
@@ -15,6 +28,7 @@ results = {
     "id": [],
     "filename": [],
     "Channel": [],
+    "sex": [],
     "pk_min_base": [],
     "pk_min_fluo": [],
     "pk_min_RKCN": [],
@@ -50,69 +64,75 @@ results = {
     "std_min_levi_55_60": [],
     "amp_min_pera_55_60": [],
     "std_min_pera_55_60": [],
-    "sex": [],
+    
 }
 
-   
-for i, value in enumerate(df.values):
-    try:
-        meta = extract_df_value(value, PATH)
-        # Load data:
-        print(f"\n{'#'*100}")
-        print(f"Working on: {meta['filename']}")
-        dt = Dataset(meta)
-        treats = meta["treatments"]
-        results["id"].append(i)
-        results["filename"].append(meta["filename"].split("\\")[-1])
-        results["Channel"].append(meta["channel"])
+print('Running batch analysis ..')
+for i, value in enumerate(tqdm(df.values)):
+    # try:
+    meta = extract_df_value(value, PATH)
+    # Load data:
+    logging.info(f"\n{'#'*100}")
+    logging.info(f"Working on: {meta['filename']}")
+    dt = Dataset(meta)
 
-        results["pk_min_base"].append(dt.get_treatment_data("Baseline", minutes))
-        results["pk_min_fluo"].append(dt.get_treatment_data("Fluorocitrate", minutes))
-        results["pk_min_RKCN"].append(dt.get_treatment_data("R_KCN", minutes))
-        results["pk_min_levi"].append(dt.get_treatment_data("Levetiracetam", minutes))
-        results["pk_min_pera"].append(dt.get_treatment_data("Perampanel", minutes))
+## Logging results
+    treats = meta["treatments"]
+    results["id"].append(i)
+    results["filename"].append(meta["filename"].split("\\")[-1])
+    results["Channel"].append(meta["channel"])
+    results["sex"].append(meta["sex"])
+### peaks/min
+    results["pk_min_base"].append(dt.get_treatment_data("Baseline", minutes))
+    results["pk_min_fluo"].append(dt.get_treatment_data("Fluorocitrate", minutes))
+    results["pk_min_RKCN"].append(dt.get_treatment_data("R_KCN", minutes))
+    results["pk_min_levi"].append(dt.get_treatment_data("Levetiracetam", minutes))
+    results["pk_min_pera"].append(dt.get_treatment_data("Perampanel", minutes))
+    results["pk_min_RKCN_25_30"].append(dt.get_treatment_data("R_KCN", minutes, time="25_30"))
+    results["pk_min_levi_25_30"].append(dt.get_treatment_data("Levetiracetam", minutes, time="25_30"))
+    results["pk_min_pera_25_30"].append(dt.get_treatment_data("Perampanel", minutes, time="25_30"))
+    results["pk_min_RKCN_55_60"].append(dt.get_treatment_data("R_KCN", minutes, time="55_60"))
+    results["pk_min_levi_55_60"].append(dt.get_treatment_data("Levetiracetam", minutes, time="55_60"))
+    results["pk_min_pera_55_60"].append(dt.get_treatment_data("Perampanel", minutes, time="55_60"))
+### mean amplitude
+    results["amp_min_base"].append(dt.get_treatment_data("Baseline", minutes, stats="mean"))
+    results["amp_min_fluo"].append(dt.get_treatment_data("Fluorocitrate", minutes, stats="mean"))
+    results["amp_min_RKCN"].append(dt.get_treatment_data("R_KCN", minutes, stats="mean"))
+    results["amp_min_levi"].append(dt.get_treatment_data("Levetiracetam", minutes, stats="mean"))
+    results["amp_min_pera"].append(dt.get_treatment_data("Perampanel", minutes, stats="mean"))
+    results["amp_min_RKCN_25_30"].append(dt.get_treatment_data("R_KCN", minutes, time="25_30", stats="mean"))
+    results["amp_min_levi_25_30"].append(dt.get_treatment_data("Levetiracetam", minutes, time="25_30", stats="mean"))
+    results["amp_min_pera_25_30"].append(dt.get_treatment_data("Perampanel", minutes, time="25_30", stats="mean"))
+    results["amp_min_RKCN_55_60"].append(dt.get_treatment_data("R_KCN", minutes, time="55_60", stats="mean"))
+    results["amp_min_levi_55_60"].append(dt.get_treatment_data("Levetiracetam", minutes, time="55_60", stats="mean"))
+    results["amp_min_pera_55_60"].append(dt.get_treatment_data("Perampanel", minutes, time="55_60", stats="mean"))
+### std amplitude
+    results["std_min_base"].append(dt.get_treatment_data("Baseline", minutes, stats="std"))
+    results["std_min_fluo"].append(dt.get_treatment_data("Fluorocitrate", minutes, stats="std"))
+    results["std_min_pera"].append(dt.get_treatment_data("Perampanel", minutes, stats="std"))
+    results["std_min_RKCN"].append(dt.get_treatment_data("R_KCN", minutes, stats="std"))
+    results["std_min_levi"].append(dt.get_treatment_data("Levetiracetam", minutes, stats="std"))
+    results["std_min_RKCN_25_30"].append(dt.get_treatment_data("R_KCN", minutes, time="25_30", stats="std"))
+    results["std_min_levi_25_30"].append(dt.get_treatment_data("Levetiracetam", minutes, time="25_30", stats="std"))
+    results["std_min_pera_25_30"].append(dt.get_treatment_data("Perampanel", minutes, time="25_30", stats="std"))
+    results["std_min_RKCN_55_60"].append(dt.get_treatment_data("R_KCN", minutes, time="55_60", stats="std"))
+    results["std_min_levi_55_60"].append(dt.get_treatment_data("Levetiracetam", minutes, time="55_60", stats="std"))
+    results["std_min_pera_55_60"].append(dt.get_treatment_data("Perampanel", minutes, time="55_60", stats="std"))
+    savename = meta["filename"].split("\\")[-1][:-4] + "_Ch" + str(meta["channel"])
+    dt.plot_peaks(f"plot_results/{savename}")
+    # dt.plot_peaks(f"plot_results/test")
+    # break
+    # except:
+    #     print(f"Failed on: {meta['filename']}")
 
-        results["pk_min_RKCN_25_30"].append(dt.get_treatment_data("R_KCN", minutes, time="25_30"))
-        results["pk_min_levi_25_30"].append(dt.get_treatment_data("Levetiracetam", minutes, time="25_30"))
-        results["pk_min_pera_25_30"].append(dt.get_treatment_data("Perampanel", minutes, time="25_30"))
-        results["pk_min_RKCN_55_60"].append(dt.get_treatment_data("R_KCN", minutes, time="55_60"))
-        results["pk_min_levi_55_60"].append(dt.get_treatment_data("Levetiracetam", minutes, time="55_60"))
-        results["pk_min_pera_55_60"].append(dt.get_treatment_data("Perampanel", minutes, time="55_60"))
-    # ####
-        results["amp_min_base"].append(dt.get_treatment_data("Baseline", minutes, stats="mean"))
-        results["amp_min_fluo"].append(dt.get_treatment_data("Fluorocitrate", minutes, stats="mean"))
-        results["amp_min_RKCN"].append(dt.get_treatment_data("R_KCN", minutes, stats="mean"))
-        results["amp_min_levi"].append(dt.get_treatment_data("Levetiracetam", minutes, stats="mean"))
-        results["amp_min_pera"].append(dt.get_treatment_data("Perampanel", minutes, stats="mean"))
 
-        results["amp_min_RKCN_25_30"].append(dt.get_treatment_data("R_KCN", minutes, time="25_30", stats="mean"))
-        results["amp_min_levi_25_30"].append(dt.get_treatment_data("Levetiracetam", minutes, time="25_30", stats="mean"))
-        results["amp_min_pera_25_30"].append(dt.get_treatment_data("Perampanel", minutes, time="25_30", stats="mean"))
-        results["amp_min_RKCN_55_60"].append(dt.get_treatment_data("R_KCN", minutes, time="55_60", stats="mean"))
-        results["amp_min_levi_55_60"].append(dt.get_treatment_data("Levetiracetam", minutes, time="55_60", stats="mean"))
-        results["amp_min_pera_55_60"].append(dt.get_treatment_data("Perampanel", minutes, time="55_60", stats="mean"))
-
-        results["std_min_base"].append(dt.get_treatment_data("Baseline", minutes, stats="std"))
-        results["std_min_fluo"].append(dt.get_treatment_data("Fluorocitrate", minutes, stats="std"))
-        results["std_min_pera"].append(dt.get_treatment_data("Perampanel", minutes, stats="std"))
-        results["std_min_RKCN"].append(dt.get_treatment_data("R_KCN", minutes, stats="std"))
-        results["std_min_levi"].append(dt.get_treatment_data("Levetiracetam", minutes, stats="std"))
-
-        results["std_min_RKCN_25_30"].append(dt.get_treatment_data("R_KCN", minutes, time="25_30", stats="std"))
-        results["std_min_levi_25_30"].append(dt.get_treatment_data("Levetiracetam", minutes, time="25_30", stats="std"))
-        results["std_min_pera_25_30"].append(dt.get_treatment_data("Perampanel", minutes, time="25_30", stats="std"))
-        results["std_min_RKCN_55_60"].append(dt.get_treatment_data("R_KCN", minutes, time="55_60", stats="std"))
-        results["std_min_levi_55_60"].append(dt.get_treatment_data("Levetiracetam", minutes, time="55_60", stats="std"))
-        results["std_min_pera_55_60"].append(dt.get_treatment_data("Perampanel", minutes, time="55_60", stats="std"))
-        results["sex"].append(meta["sex"])
-
-#     # break
-    except:
-        print(f"Failed on: {meta['filename']}")
-print(f"RESULTS >>> {len(results)}")
 df_res = pd.DataFrame.from_dict(results)
-df_res.to_csv("peak_rate_5min_amp_std.csv")
-print(df_res.head())
+f = 'files'
+if len(df_res) <=1:
+    f = 'file'
+print(f"Done! Analysed {len(df_res)} {f}.")
+df_res.to_csv("test.csv")
+# print(df_res.head())
     
 
 # if __name__ == "__main__":
--- utils/utils.py
+++ utils/utils.py
@@ -1,10 +1,16 @@
 import neo
 import numpy as np
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 from scipy.signal import decimate, find_peaks, savgol_filter
 from plots.plots import *
+import logging
 import os
 
+plt.subplots_adjust(bottom=0.3)
+plt.rcParams.update({'font.size': 16})
+# sns.set(font_scale=1.6, style='white')
 ### Collection of class and function to load and preprocess data
 
 
@@ -48,7 +54,6 @@ def extract_df_value(value, PATH):
 
     treatments = pd.DataFrame(value[3:-2]).T
     treatments.columns = list(COLORS.keys())
-    treatments.dropna(inplace=True, axis=1)
 
     meta["treatments"] = treatments
     meta["filename"] = filename
@@ -70,8 +75,9 @@ class Dataset:
         self.data = self.data - self.data.mean()
         self.downsample()
         if self.meta["big_tresh"]:
-            print("Cleanning big tresh..")
+            logging.info("Cleanning big tresh..")
             self.clean_noise(self.meta["big_tresh"])
+        logging.info(f"Sample rate: {self.sf}")
         self.base_stats = None
         self.fluo_stats = None
         self.r_KCN_stats = None
@@ -92,21 +98,24 @@ class Dataset:
 
     def get_treatments(self):
         """Return a dataframe of treatments intervals"""
-        colors = list(self.meta["colors"].keys())
+        
+        treatments = self.meta["treatments"]
+        treatments.dropna(inplace=True, axis=1)
+        columns = treatments.columns
         treatments = list(self.meta["treatments"].values[0])
         treatments.append(int(self.data.size / self.sf))
         t_times = []
-        columns = []
         for i in range(0, len(treatments) - 1):  # get all intervals
             t_times.append((int(treatments[i]), int(treatments[i + 1])))
-            columns.append(colors[i])
+            # columns.append(columns[i])
         res = pd.DataFrame(t_times).T
         res.columns = columns
+        logging.info(f"Treatments (start-end): \n{res}")
         return res
 
     def read_data(self):
         """Read Spike2 file"""
-        print("Reading file:")
+        logging.info("Reading file:")
         reader = neo.io.Spike2IO(
             filename=self.meta["filename"], try_signal_grouping=False
         )
@@ -157,105 +166,167 @@ class Dataset:
         for idx in pos_idxs:
             self.data[idx - int(self.sf // 2) : idx + int(self.sf // 2)] = 0
 
-        print(f"Removed {n_removed} peaks.")
+        logging.info(f"Removed {n_removed} peaks.")
         pass
+
 
     def get_peaks(self):
         """Find sample peaks"""
         w = 33
-        dist = 10  # minimal dist in samples between peaks
-        width = [3]  # minimal width of a peak in samples
+        dist = 20
+        width = [20]  # minimal width of a peak in samples
         # smooth the data to avoid high frequency peaks (spikes)
         f_data = savgol_filter(self.data.copy(), window_length=w, polyorder=1, deriv=0)
 
         peaks_neg = find_peaks(
             -f_data, height=self.meta["small_tresh"], width=width, distance=dist
         )[0]
+        logging.info(f"Found total of {len(peaks_neg)} peaks")
         return np.asarray(peaks_neg)
 
-    def get_peaks_by_interval(self, t):
+
+    def get_peaks_by_interval(self, data_time):
         '''return the peaks around the interval t[0] to t[1]'''
         peaks = self.get_peaks()
-        return peaks[(peaks > t[0] * self.sf) & (peaks <= t[-1] * self.sf)]
+        peaks_interval =  peaks[(peaks > data_time[0] * self.sf) & (peaks <= data_time[1] * self.sf)]
+        logging.info(f"Found {len(peaks_interval)} peaks in the interval {data_time[0]} to {data_time[1]} => {(data_time[1]-data_time[0])/60} min")
+        return peaks_interval
 
-    def get_peaks_min(self, t, minutes):
+    def get_peaks_min(self, data_time, minutes):
         ''' Get the number of peaks per min'''
-        pks = self.get_peaks_by_interval(t)
-        return len(pks) / minutes
+        pks = self.get_peaks_by_interval(data_time)
+        pks_min =  len(pks) / minutes
+        logging.info(f"Peaks per min: {pks_min} in {minutes} minutes")
+        return pks_min
 
-    def calculate_amplitudes_mean_std(self, t,  stats="mean"):
+
+    def calculate_amplitudes_mean_std(self, data_time,  stats="mean"):
         """Calculate mean or Std amplitudes of peaks inside interval t"""
-        pk = self.get_peaks_by_interval(t)
-        if len(pk) == 0:
+        pk = self.get_peaks_by_interval(data_time)
+        if len(pk) < 3:
+            logging.info(f"Number of peaks < 3, stats will not be calculated.")
             return 0
         amp = self.data[pk]
         if stats == "std":
             return amp.std()
+        logging.info(f"Calculating amplitude {stats} for {len(pk)} peaks: {amp.mean()} in {amp.std()} minutes")
         return amp.mean()
 
-    def get_treatment_in_minutes(self, treatment_name):
-        '''Get the entire treatment time split in minutes'''
 
+    def get_treatment_interval(self, treatment_name):
+        '''Get the entire treatment time split in seconds'''
         try:  # some files dont have all treatments
             t_times = self.get_treatments()[treatment_name].values
+            logging.info(f"FOUND treatment data:  {treatment_name}.")    
         except:
+            logging.info(f"NO TREATMENT DATA:  {treatment_name}.")
             return None
-        times_s = np.arange(t_times[0], t_times[1])  # get the interval between t0 - t1
-        times_m = np.array_split(times_s, int((t_times[1] - t_times[0]) // 60))
-        return times_m
-
-
-    def get_baseline_data(self, minutes=5, stats="mean"):
-        '''Get Baseline treatment peaks/min or mean amplitude or std on the minutes interval'''
-        times_m = self.get_treatment_in_minutes("Baseline")
-        if len(times_m) < 5:
-            t = [times_m[0][-1], times_m[-1][-1]]
-        else:
-            t = [times_m[-minutes][0], times_m[-1][-1]]
-
-        if stats:
-            return self.calculate_amplitudes_mean_std(t, stats)
         
-        return self.get_peaks_min(t, minutes)        
+        return t_times
+
+
+    # def get_baseline_data(self, minutes, stats="mean"):
+    #     '''Get Baseline treatment peaks/min or mean amplitude or std on the minutes interval'''
+        
+    #     t_times = self.get_treatment_interval("Baseline")
+    #     interval = t_times[1] - t_times[0]
+    #     seconds = minutes * 60
+
+    #     if interval < seconds:
+    #         data_time = t_times
+    #     else:
+    #         data_time = [(t_times[1] - seconds), t_times[-1]]
+    #         logging.info(f'Time for calculation {(data_time[0])/60:.1f} to {(data_time[1])/60:.1f} => {(data_time[1]-data_time[0])/60} min')
+
+    #     if stats:
+    #         return self.calculate_amplitudes_mean_std(data_time, stats)
+        
+    #     return self.get_peaks_min(data_time, minutes)        
 
 
     def get_treatment_data(self, treatment, minutes, stats=None, time=""):
+
+        logging.info(f"\n\n{treatment}, stats: {stats}, time: {time}")
+        t_times = self.get_treatment_interval(treatment)
         
-        times_m = self.get_treatment_in_minutes(treatment)
-        if times_m is None:
+        if t_times is None:
             return None
-
+        interval = t_times[1] - t_times[0]
+        seconds = minutes * 60
+        logging.info(f"Time total: t_times {t_times[0]/60:.1f} to {t_times[1]/60:.1f} => {interval/60:.2f} min")
+        
         if treatment == "Baseline":
-            return self.get_baseline_data(minutes=5, stats=stats)
-
-
-        times_m = self.get_treatment_in_minutes(treatment)
-        if time == "25_30":
-            if len(times_m) >= 30:
-                t = [times_m[24][0], times_m[29][-1]]
-                if stats:
-                    return self.calculate_amplitudes_mean_std(t, stats)
-                return self.get_peaks_min(t, minutes)
+            # return self.get_baseline_data(minutes, stats=stats)
+            if interval < seconds:
+                data_time = t_times
             else:
-                t = [times_m[-minutes][0], times_m[-1][-1]]
+                data_time = [(t_times[1] - seconds), t_times[-1]]
+                logging.info(f'Time for calculation {(data_time[0])/60:.1f} to {(data_time[1])/60:.1f} => {(data_time[1]-data_time[0])/60} min')
+            if stats:
+                return self.calculate_amplitudes_mean_std(data_time, stats)
+            return self.get_peaks_min(data_time, minutes)        
+
+        if time == "25_30":
+            if interval >= 30:
+                data_time = [(t_times[0] + (30*60 - seconds)), (t_times[0] + 30*60)]
+                logging.info(f'Time for calculation = 25_30 {(data_time[0])/60:.1f} to {(data_time[1])/60:.1f} => {(data_time[1]-data_time[0])/60} min')
                 if stats:
-                    return self.calculate_amplitudes_mean_std( t, stats)
-                return self.get_peaks_min(t, minutes)
+                    return self.calculate_amplitudes_mean_std(data_time, stats)
+                return self.get_peaks_min(data_time, minutes)
+            else:
+                data_time = [(t_times[1] - seconds) , t_times[1]]
+                logging.info(f'Time for calculation < 25_30 {(data_time[0])/60:.1f} to {(data_time[1])/60:.1f} => {(data_time[1]-data_time[0])/60} min')
+                if stats:
+                    return self.calculate_amplitudes_mean_std( data_time, stats)
+                return self.get_peaks_min(data_time, minutes)
 
         elif time == "55_60":  # get the 55-60 min or the last 5 smaller 60
-            if len(times_m) >= 60:
-                t = [times_m[54][0], times_m[59][-1]]
+            if interval >= 60:
+                data_time = [(t_times[0] + (60*60 - seconds)), (t_times[0] + 60*60)]
+                logging.info(f'Time for calculation = 55_60 {(data_time[0])/60:.1f} to {(data_time[1])/60:.1f} => {(data_time[1]-data_time[0])/60} min')
                 if stats:
-                    return self.calculate_amplitudes_mean_std( t, stats)
-                return self.get_peaks_min(t, minutes)
-            else:
-                t = [times_m[-minutes][0], times_m[-1][-1]]
+                    return self.calculate_amplitudes_mean_std(data_time, stats)
+                return self.get_peaks_min(data_time, minutes)
+            elif interval > 45:
+                data_time = [(t_times[1] - seconds) , t_times[1]]
+                logging.info(f'Time for calculation > 55_60 {(data_time[0])/60:.1f} to {(data_time[1])/60:.1f} => {(data_time[1]-data_time[0])/60} min')
                 if stats:
-                    return self.calculate_amplitudes_mean_std(t, stats)
-                return self.get_peaks_min(t, minutes)
-
+                    return self.calculate_amplitudes_mean_std(data_time, stats)
+                return self.get_peaks_min(data_time, minutes)
+            
         else:
-            t = [times_m[-minutes][0], times_m[-1][-1]]
+            data_time = [(t_times[1] - seconds), t_times[1]]
+            logging.info(f'Time for calculation last 5 min {(data_time[0])/60:.1f} to {(data_time[1])/60:.1f} => {(data_time[1]-data_time[0])/60} min')
             if stats:
-                return self.calculate_amplitudes_mean_std( t, stats)
-            return self.get_peaks_min(t, minutes)
+                return self.calculate_amplitudes_mean_std(data_time, stats)
+            return self.get_peaks_min(data_time, minutes)
+
+    def plot_peaks(self, save_name=None):
+
+
+        tvalues = self.meta['treatments'].values[0]
+        tnames = self.meta['treatments'].columns
+        tvalues = [t*self.sf for t in tvalues]
+        neg = self.get_peaks()
+        time = np.arange(0, self.data.size)/self.sf
+        sns.set_style("whitegrid", {'axes.grid' : False})
+        dy = savgol_filter(self.data, window_length=33, polyorder=1, deriv=0)
+        
+        _ , ax = plt.subplots(nrows=2, sharex=True, figsize=(20,12), 
+                            gridspec_kw={'height_ratios': [5,1]})
+
+        ax[0].plot(dy)
+        ax[0].plot(neg, dy[neg], "x")
+        ax[0].set_ylabel('Amplitude (uV)')
+        ax[1].eventplot(positions=neg, data=dy, linewidths=.4, 
+                        linelengths=.4)
+        ax[1].set_xlim(0, dy.size)       
+        ax[1].set_xlabel('Time (s)')
+        ax[1].set_ylabel('Events')
+        ax[1].set_xticks(ticks=tvalues, labels=tnames, rotation=-30)  
+
+        if save_name:
+            plt.savefig(f'{save_name}.jpg')
+        else:
+            plt.show()
+        plt.close()
